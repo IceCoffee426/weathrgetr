5d98f6df8bffddb83871d1ed480ea3fcc5eaadbe
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-04-30 17:02:55 +0100

    Add local storage and rearrange folders

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import "./style.css";
-import CODE from "./utils/config.js";
+import API_KEY from "./utils/key.js";
 
-const weather = {
-  units: "C",
-  currentLocation: "london",
+const weather = (function () {
+  let units = localStorage.getItem("units")
+    ? localStorage.getItem("units")
+    : "C";
+  let currentLocation = "london";
 
-  getLocation: function () {
+  const getLocation = function () {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
@@ -19,88 +21,94 @@ const weather = {
         weather.fetchWeather("london");
       }
     );
-  },
+  };
 
-  fetchWeather: async function (loc) {
+  const fetchWeather = async function (loc) {
     try {
-      const key = CODE;
+      const key = API_KEY;
       const response = await axios.get(
         `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=3`
       );
 
       const { data } = response;
       const { day } = response.data.forecast.forecastday[1];
-      this.displayWeather(data, day);
+      weather.displayWeather(data, day);
     } catch (err) {
-      this.logError();
+      weather.logError();
       console.log(err);
     }
-  },
+  };
 
-  displayWeather: function (today, tomorr) {
+  const displayWeather = function (today, tomorr) {
     document.querySelector(".error").style.display = "none";
     document.querySelector(".loading").style.display = "none";
     document.querySelector(".weather").style.visibility = "visible";
     document.body.style.background = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url("https://source.unsplash.com/1600x900/?${today.location.name}")`;
-    this.writeData(".city", today.location.name);
-    this.writeData(
+    weather.writeData(".city", today.location.name);
+    weather.writeData(
       ".temp",
       `${
-        this.units === "C"
+        weather.units === "C"
           ? today.current.temp_c + "\u00b0C"
           : today.current.temp_f + "\u00b0F"
       }`
     );
-    this.writeData(".icon", today.current.condition.icon);
-    this.writeData(".description", today.current.condition.text);
-    this.writeData(".humidity", `${today.current.humidity}%`);
-    this.writeData(".wind", `${today.current.wind_mph} mph`);
+    weather.writeData(".icon", today.current.condition.icon);
+    weather.writeData(".description", today.current.condition.text);
+    weather.writeData(".humidity", `${today.current.humidity}%`);
+    weather.writeData(".wind", `${today.current.wind_mph} mph`);
 
-    this.writeData(
+    weather.writeData(
       ".temp-tomorrow",
       `${
-        this.units === "C"
+        weather.units === "C"
           ? tomorr.avgtemp_c + "\u00b0C"
           : tomorr.avgtemp_f + "\u00b0F"
       }`
     );
-    this.writeData(".icon-tomorrow", tomorr.condition.icon);
-    this.writeData(".description-tomorrow", tomorr.condition.text);
-    this.writeData(".humidity-tomorrow", `${tomorr.avghumidity}%`);
-    this.writeData(".wind-tomorrow", `${tomorr.maxwind_mph} mph`);
-  },
+    weather.writeData(".icon-tomorrow", tomorr.condition.icon);
+    weather.writeData(".description-tomorrow", tomorr.condition.text);
+    weather.writeData(".humidity-tomorrow", `${tomorr.avghumidity}%`);
+    weather.writeData(".wind-tomorrow", `${tomorr.maxwind_mph} mph`);
+  };
 
-  writeData: function (className, data) {
+  const writeData = function (className, data) {
     const element = document.querySelector(className);
     if (element.nodeName === "IMG") {
       element.src = data;
       return;
     }
     element.textContent = data;
-  },
+  };
 
-  logError: function () {
+  const logError = function () {
     document.querySelector(".loading").style.display = "none";
     document.querySelector(".error").style.display = "block";
-  },
+  };
 
-  getUnits: function () {
+  const getUnits = function () {
     const toggle = document.getElementById("toggle-units");
+    if (localStorage.getItem("units") === "F") {
+      toggle.checked = true;
+      document.querySelector(".units").innerText = weather.units;
+    }
     toggle.addEventListener("click", () => {
       if (toggle.checked) {
         weather.units = "F";
         weather.fetchWeather(weather.currentLocation);
-        document.querySelector(".units").innerText = "F";
+        document.querySelector(".units").innerText = weather.units;
+        localStorage.setItem("units", "F");
       } else {
         weather.units = "C";
         weather.fetchWeather(weather.currentLocation);
-        document.querySelector(".units").innerText = "C";
+        document.querySelector(".units").innerText = weather.units;
+        localStorage.setItem("units", "C");
       }
     });
-  },
+  };
 
-  search: function () {
+  const search = function () {
     const searchBtn = document.getElementById("search-btn");
     searchBtn.addEventListener("click", function (e) {
       e.preventDefault();
@@ -109,9 +117,9 @@ const weather = {
       weather.currentLocation = location;
       weather.fetchWeather(location);
     });
-  },
+  };
 
-  toggleDay: function () {
+  const toggleDay = function () {
     const toggleDay = document.getElementById("toggle-day");
     toggleDay.addEventListener("click", function () {
       const today = document.querySelector(".weather");
@@ -124,8 +132,21 @@ const weather = {
         toggleDay.textContent = "Tomorrow";
       else toggleDay.textContent = "Today";
     });
-  },
-};
+  };
+
+  return {
+    units,
+    currentLocation,
+    getLocation,
+    fetchWeather,
+    displayWeather,
+    writeData,
+    logError,
+    getUnits,
+    search,
+    toggleDay,
+  };
+})();
 
 weather.getUnits();
 weather.search();
